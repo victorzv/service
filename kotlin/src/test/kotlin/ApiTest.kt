7bd8f36cda918c39b0c9d91754bcf915f0235717
victorzv <kaplantigra@gmail.com>
2024-05-09 10:03:22 +0300

    test single ref kotlin

--- kotlin/src/test/kotlin/ApiTest.kt
+++ kotlin/src/test/kotlin/ApiTest.kt
@@ -1,7 +1,11 @@
 
 import io.swagger.client.apis.ApiApi
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.asRequestBody
 import org.junit.Test
 import java.io.File
+import java.io.IOException
 
 class ApiApiTest {
 
@@ -13,6 +17,49 @@ class ApiApiTest {
         val fileBytes = file.readBytes()
         val files = arrayOf(fileBytes.toTypedArray())
         val response = api.pdfWebapiComparisonPost(files)
-        println(response)
+        println("Response Code: ${response.code}")
+        println("Response Message: ${response.message}")
+        println("Response Headers: ${response.headers}")
+
+// Вывод тела ответа
+        val responseBody = response.body?.string()
+        println("Response Body: $responseBody")
+    }
+
+    @Test
+    fun testManualUpload(){
+        val url = "http://localhost:5000/pdf/webapi/Comparison"
+
+        val filePath = "/home/tigra/Service/TestData/test.pdf"
+
+        val file = File(filePath)
+
+        val client = OkHttpClient()
+
+        val requestBody = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart(
+                "file",
+                file.name,
+                file.asRequestBody("application/pdf".toMediaTypeOrNull())
+            )
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        try {
+            val response = client.newCall(request).execute()
+            if (response.isSuccessful) {
+                println("Response received: ${response.code}")
+                println(response.body?.string())
+            } else {
+                println("Request failed: ${response.code}")
+            }
+        } catch (e: IOException) {
+            println("Request failed: ${e.message}")
+        }
     }
 }
